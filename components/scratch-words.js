9028f80951c2310cc19311a9be873b4b95ed6ac7
zhoushan <672563682@qq.com>
2023-03-28 19:58:37 +0800

    feat: init

---
+++ components/scratch-words.js
@@ -0,0 +1,4 @@
+// const { globalShortcut } = require('electron')
+// globalShortcut.register('CommandOrControl+Shift+X', () => {
+//   console.log('Mouse event detected')
+// })
--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
+require('./components/scratch-words')
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+    },
   })
 
   // and load the index.html of the app.
