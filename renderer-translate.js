217241a6b5bd47486377619427b72a56ffa61ea7
zhoushan <672563682@qq.com>
2023-03-31 10:34:30 +0800

    fix: add

--- renderer-translate.js
+++ renderer-translate.js
@@ -3,15 +3,12 @@ const { ipcRenderer } = require('electron')
 
 window.onload = () => {
   window.getText = () => document.getElementById('myTextArea').value
-  window.setText = (text) =>
-    (document.getElementById('myTextArea').innerHTML = text)
-
-  // 从主进程中获取TextArea的值
-  // const text = ipcRenderer.sendSync('get-text')
-  // // 将值设置到TextArea中
-  // window.setText(text)
+  window.setText = (text) => {
+    document.getElementById('myTextArea').innerHTML = text
+  }
 
   window.getToText = () => document.getElementById('toTextArea').value
-  window.setToText = (text) =>
-    (document.getElementById('toTextArea').innerHTML = text)
+  window.setToText = (text) => {
+    document.getElementById('toTextArea').innerHTML = text
+  }
 }
